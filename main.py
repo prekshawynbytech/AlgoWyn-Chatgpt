451edad7e8ef4abe2a5db6af9b9ef0eb7cbf7f77
prekshawynbytech <preksha@wynbytech.com>
2024-07-16 15:19:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ custom_css = """
                 background-color: white;
             }
             .title {
-                position:fixed;
                 color: #e2472f;
                 font-family: 'Helvetica Neue', Arial;
                 font-size: 2em;
