c241b5b949cea0a7ab1937c7489a0e0ca3421462
prekshawynbytech <preksha@wynbytech.com>
2024-07-16 15:32:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(page_title="AlgoWyn", page_icon=None, layout="centered", initial_sidebar_state="auto")
 
 #st.image("logo.png", width=100)
-logo = Image.open("logo.png")
+#logo = Image.open("logo.png")
 custom_css = """
             <style>
              body {
