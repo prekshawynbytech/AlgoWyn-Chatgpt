5c26c729326528f1257e00216bcff07740b24bda
prekshawynbytech <preksha@wynbytech.com>
2024-07-17 16:43:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ st.markdown(
          font-family: 'Helvetica Neue', Arial;
         font-size: 2em;
         font-weight: bold;
-        margin-left:200px;
+        margin-left:150px;
         }
     </style>
     <div class="header">
