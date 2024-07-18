8c63ac738922e73b529ed9107c5b3cae754edafe
prekshawynbytech <preksha@wynbytech.com>
2024-07-18 11:04:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ st.markdown(
     }
     .title {
         color: #e2472f;
+        position:absolute;
         font-family: 'Helvetica Neue', Arial;
         font-size: 2em;
         font-weight: bold;
